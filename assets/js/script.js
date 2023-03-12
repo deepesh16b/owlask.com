c16a45ee8f2b9836c0245149a3ab31a8fc0d5d83
Deepesh Bhardwaj <70226498+deepesh16b@users.noreply.github.com>
2023-03-12 23:09:10 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,14 @@ function toggle() {
   blur.forEach((e) => {
     e.classList.toggle("active");
   });
+
   var all_popups = document.querySelectorAll(".popup");
   all_popups.forEach((e) => {
+    e.classList.remove("active");
+  });
+
+  var all_popups_recents = document.querySelectorAll(".popup-recents");
+  all_popups_recents.forEach((e) => {
     e.classList.remove("active");
   });
 }
@@ -19,11 +25,12 @@ heading_a.forEach((e) => {
   });
 });
 
-// var close_a = document.querySelectorAll(".close-a");
-// close_a.forEach( e => {
-//     e.addEventListener("click", (event) => {
-//         console.log(event.target.parentNode.parentNode.parentNode.parentNode);
-//         var parent_div = event.target.parentElement.parentElement.parentElement.parentNode;
-//         parent_div.querySelector(".popup").classList.toggle("active");
-//     });
-// });
+var recent_heading_a = document.querySelectorAll(".recent-heading-a");
+recent_heading_a.forEach((e) => {
+  e.addEventListener("click", (event) => {
+    console.log(event.target.parentNode.parentNode);
+    var parent_div =
+      event.target.parentElement.parentElement;
+    parent_div.querySelector(".popup-recents").classList.toggle("active");
+  });
+});
